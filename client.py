819551a0efe2c3922e87290c2bf00a9dee613c36
Marco Guidi <mark.88g@gmail.com>
2022-04-18 12:37:54 +0200

    Added check on CRL

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
     CLIENT script:
         1) Load CA, server cert and CA revocation list
             and verify server cert validity
+
     @author: Marco Guidi
 """
 
@@ -17,6 +18,13 @@ from cryptography.x509.oid import NameOID
 
 import sys
 import datetime
+
+# =============================================================================
+# GLOBAL CONST
+# =============================================================================
+
+NEW_LINE = "\r\n"
+CHECK_REVOCATION_LIST = True
 
 # =============================================================================
 # SCRIPT 1
@@ -45,10 +53,22 @@ server_pub_key = server_cert.public_key()
 now = datetime.datetime.now()
 # now = datetime.datetime(2030, 5, 17)
 
+# Load CA crl
+with open( "fake_CA_crl.pem", "rb" ) as f:
+    pem_text = f.read()
+    ca_crl = x509.load_pem_x509_crl( pem_text, default_backend() )
+
+revoked_cert = ca_crl.get_revoked_certificate_by_serial_number( int(server_cert.serial_number) )
+
 # Validation and verify
-if not ( server_issuer_name == ca_name and
-    server_cert.not_valid_before <= now <= server_cert.not_valid_after ):
-    print( "Error. Server certificate is not valid!" )
+if not server_issuer_name == ca_name:
+    print( f"{NEW_LINE}ERROR: Server certificate is not valid!" )
+    sys.exit()
+elif not server_cert.not_valid_before <= now <= server_cert.not_valid_after:
+    print( f"{NEW_LINE}ERROR: Server certificate is expired!" )
+    sys.exit()
+elif CHECK_REVOCATION_LIST and not revoked_cert == None:
+    print( f"{NEW_LINE}ERROR: Server certificate has been revoked!" )
     sys.exit()
 
 ca_pub_key.verify(
