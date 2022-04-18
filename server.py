68a2ab87c515d6baea356530798daa61555b497e
Marco Guidi <mark.88g@gmail.com>
2022-04-18 15:32:44 +0200

    Added padding

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 """
     SERVER script:
         1) Get the msg and encrypt with AES-128-CBC
+        2) Load PK and sign the message
 
     @author: Marco Guidi
 """
@@ -9,9 +10,10 @@
 # PACKAGES
 # =============================================================================
 
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 import binascii
 import os
@@ -22,22 +24,29 @@ import sys
 # =============================================================================
 
 NEW_LINE = "\r\n"
-BLOCK_SIZE = int( algorithms.AES.block_size / 8 )
+BLOCK_SIZE_BITS = algorithms.AES.block_size
+
+SERVER_PK = "fake_server_key.pem"
 
 # =============================================================================
 # SCRIPT 1
 # =============================================================================
 
 AES_KEY = binascii.unhexlify("506C616E74207472506C616E74207472")
-AES_IV = os.urandom( BLOCK_SIZE )
+AES_IV = os.urandom( int( BLOCK_SIZE_BITS / 8 ) )
 
 print( f"{NEW_LINE}Loaded AES key and generated AES IV." )
 
 plain_text = input( "Write the message to send to the client: " ).encode()
 
 cipher = Cipher( algorithms.AES( AES_KEY ), modes.CBC( AES_IV ), default_backend() )
+
+# Add padding
+padder = padding.PKCS7( BLOCK_SIZE_BITS ).padder()
+padded_plain_text = padder.update( plain_text ) + padder.finalize()
+
+# Cypher the padded text
 ctx = cipher.encryptor()
-ctx = padding.PKCS7( BLOCK_SIZE ).padder()
-ciphertext = ctx.update( plain_text ) + ctx.finalize()
+ciphertext = ctx.update( padded_plain_text ) + ctx.finalize()
 
 print( f"{NEW_LINE}Message encrypted. Message: {plain_text}, cypher text: {ciphertext}." )
