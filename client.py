bb89f3ad306f44ee8f6d6167dc847079fc1d03b8
Marco Guidi <mark.88g@gmail.com>
2022-04-18 18:59:15 +0200

    Added decrypt

--- client.py
+++ client.py
@@ -3,6 +3,7 @@
         1) Load CA, server cert and CA revocation list
             and verify server cert validity
         2) Verify that the signed-msg is from the server
+        3) Decript message
 
     @author: Marco Guidi
 """
@@ -11,14 +12,18 @@
 # PACKAGES
 # =============================================================================
 
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding as padding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import padding as padding_asym
+
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 
 import sys
 import datetime
+import binascii
 
 # =============================================================================
 # GLOBAL CONST
@@ -34,8 +39,12 @@ NOW = datetime.datetime.now() # invalid time: datetime.datetime(2030, 5, 17)
 CA_CRL = "fake_CA_crl.pem"
 CHECK_REVOCATION_LIST = False # set True if you want to check CA crl
 
-ENC_FILE = "msg.enc"
+ENC_FILE = "msg.txt"
 SIGN_FILE = "msg.sgn"
+
+AES_KEY = binascii.unhexlify("506C616E74207472506C616E74207472")
+
+BLOCK_SIZE_BITS = algorithms.AES.block_size
 
 # =============================================================================
 # SCRIPT 1
@@ -85,7 +94,7 @@ elif CHECK_REVOCATION_LIST and not revoked_cert == None:
 ca_pub_key.verify(
     server_cert.signature,
     server_cert.tbs_certificate_bytes,
-    padding.PKCS1v15(),
+    padding_asym.PKCS1v15(),
     hashes.SHA256()
 )
 
@@ -96,7 +105,8 @@ print( f"{NEW_LINE}The certificate of {server_name} is valid!" )
 # =============================================================================
 
 with open( ENC_FILE, "rb" ) as f:
-    enc_msg = f.read()
+    iv = f.read( int( BLOCK_SIZE_BITS / 8 ) )
+    padded_cipher_text = f.read()    
 
 with open( SIGN_FILE, "rb" ) as f:
     signature = f.read()
@@ -104,10 +114,10 @@ with open( SIGN_FILE, "rb" ) as f:
 try:
     server_pub_key.verify(
         signature,
-        enc_msg,
-        padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
+        iv + padded_cipher_text,
+        padding_asym.PSS(
+            mgf=padding_asym.MGF1(hashes.SHA256()),
+            salt_length=padding_asym.PSS.MAX_LENGTH
         ),
         hashes.SHA256()
     )
@@ -115,3 +125,19 @@ try:
     print( f"{NEW_LINE}The message is verified!" )
 except:
     print( f"{NEW_LINE}ERROR: The message is not verified!" )
+
+# =============================================================================
+# SCRIPT 3
+# =============================================================================
+
+cipher = Cipher( algorithms.AES( AES_KEY ), modes.CBC( iv ), default_backend() )
+
+# Decrypt message
+ctx = cipher.decryptor()
+padded_plain_text = ctx.update( padded_cipher_text ) + ctx.finalize()
+
+# Remove padding
+unpadder = padding.PKCS7( BLOCK_SIZE_BITS ).unpadder()
+plain_text = unpadder.update( padded_plain_text ) + unpadder.finalize()
+
+print( f"{NEW_LINE}Here the message: {plain_text}" )
